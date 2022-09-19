5bf75591594e82ff6492239bc52beee89ad95424
Diogo Baltazar <ti3@ballke.com.br>
2022-09-19 13:01:36 -0300

    adicionando click na matriz para impedir erro 500 no server da uffs e retornando notas da matriz em endpoint

--- main.py
+++ main.py
@@ -30,7 +30,8 @@ def get_current_user(credentials: HTTPBasicCredentials = Depends(security)):
     browser = prepare_selenium_session(credentials.username, credentials.password)
     if browser.session:
         browser.driver.quit()
-        return User(session=browser.session, username=credentials.username, expiration=datetime.now() + timedelta(minutes=30))
+        return User(session=browser.session, username=credentials.username,
+                    expiration=datetime.now() + timedelta(minutes=30))
 
     raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
--- scrapping/data.py
+++ scrapping/data.py
@@ -7,8 +7,8 @@ from selenium.webdriver.common.by import By
 from scrapping.bs4 import ParseHTML
 from scrapping.selenium import Browser
 
+
 def notas_matriz(session: str, login=None, senha=None):
-    NAMES = list(range(12))
     if not session:
         browser = prepare_selenium_session(login, senha)
         session = browser.session
@@ -16,6 +16,7 @@ def notas_matriz(session: str, login=None, senha=None):
     soup = BeautifulSoup(response.content, features="html.parser")
     parse = ParseHTML(soup)
 
+    NAMES = parse.get_table_head('div', 'frmPrincipal:tblAcompanhamento', 'th')
     data = parse.table_json_by_id(NAMES, 'tbody', 'frmPrincipal:tblAcompanhamento_data', 'td')
     return data
 
@@ -42,7 +43,8 @@ def get_html(session: str, url: str) -> httpx.get:
 
 def prepare_selenium_session(login, senha) -> Browser:
     browser = Browser()
-    browser.driver.get('https://id.uffs.edu.br/id/XUI/#login/&realm=/&forward=true&spEntityID=uffs%3Aportalaluno%3Asp&goto=%2FSSORedirect%2FmetaAlias%2Fidp%3FReqID%3Da44j2fde4f5fd58b3j680e7ej96ddi7%26index%3Dnull%26acsURL%3Dhttps%253A%252F%252Faluno.uffs.edu.br%253A443%252Faluno%252Fsaml%252FSSO%26spEntityID%3Duffs%253Aportalaluno%253Asp%26binding%3Durn%253Aoasis%253Anames%253Atc%253ASAML%253A2.0%253Abindings%253AHTTP-POST&AMAuthCookie=')
+    browser.driver.get(
+        'https://id.uffs.edu.br/id/XUI/#login/&realm=/&forward=true&spEntityID=uffs%3Aportalaluno%3Asp&goto=%2FSSORedirect%2FmetaAlias%2Fidp%3FReqID%3Da44j2fde4f5fd58b3j680e7ej96ddi7%26index%3Dnull%26acsURL%3Dhttps%253A%252F%252Faluno.uffs.edu.br%253A443%252Faluno%252Fsaml%252FSSO%26spEntityID%3Duffs%253Aportalaluno%253Asp%26binding%3Durn%253Aoasis%253Anames%253Atc%253ASAML%253A2.0%253Abindings%253AHTTP-POST&AMAuthCookie=')
     browser.wait_page(10, 'idToken1', By.ID)
     browser.login(By.ID, 'idToken1', login, 'idToken2', senha, 'loginButton_0')
     browser.wait_page(10, 'input-username', By.ID)
@@ -53,5 +55,6 @@ def prepare_selenium_session(login, senha) -> Browser:
         browser.driver.find_element(By.PARTIAL_LINK_TEXT, 'ATIVA').click()
     except:
         pass
+    browser.driver.find_element(By.PARTIAL_LINK_TEXT, 'Acompanhamento da Matriz').click()
     browser.set_session('JSESSIONID')
     return browser
